2a73f15aa9e480b330a9365f2f0256419f90ba0b
alariej <alaj@gmx.net>
2023-06-07 11:18:12 +0200

    Added react-native-web alias in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	resolve: {
+		alias: {
+			'react-native': 'react-native-web',
+		},
+	},
   plugins: [
     react(),
     electron([
