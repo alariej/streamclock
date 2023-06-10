7a39d524c7afb0fc5a401319688fd0c1f945538c
alariej <alaj@gmx.net>
2023-06-10 13:10:20 +0200

    Added alarmSettings

---
+++ src/alarmSettings.tsx
@@ -0,0 +1,186 @@
+import { Component } from 'react';
+import { Modal, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+	APPCOLOR,
+	BUTTONCOLOR,
+	CLOSE,
+	FONTCOLOR,
+	MARGIN,
+	MODALBACKGROUNDCOLOR,
+	OPACITYPRESSED,
+	SAVE,
+	ALARMSTORAGE,
+	SettingsData,
+	TEXTINPUTBACKGROUNDCOLOR,
+	TEXTINPUTFONTCOLOR,
+	ALARMTIME,
+	DEFAULTALARMTIME,
+} from './uiconfig';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const fontSize = 16;
+// const width = 260;
+const gridMargin = 4;
+const textPadding = 4;
+
+const styles = StyleSheet.create({
+	container: {
+		position: 'absolute',
+		top: 0,
+		bottom: 0,
+		left: 0,
+		right: 0,
+		alignItems: 'center',
+		justifyContent: 'center',
+		backgroundColor: MODALBACKGROUNDCOLOR,
+	},
+	containerSettingss: {
+		backgroundColor: APPCOLOR,
+		padding: MARGIN,
+	},
+	settingsRow: {
+		flexDirection: 'row',
+		marginVertical: gridMargin / 2,
+	},
+	label: {
+		fontSize: fontSize,
+		color: FONTCOLOR,
+		width: 120,
+		textAlign: 'right',
+		marginRight: 8,
+		padding: 4,
+	},
+	alarmInput: {
+		flex: 1,
+		// width: width,
+		fontSize: 16,
+		color: TEXTINPUTFONTCOLOR,
+		padding: textPadding,
+		backgroundColor: TEXTINPUTBACKGROUNDCOLOR,
+		marginRight: MARGIN,
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+		marginTop: MARGIN,
+	},
+	button: {
+		alignItems: 'center',
+		justifyContent: 'center',
+		height: 32,
+		width: 140,
+		borderRadius: 32 / 2,
+		marginHorizontal: MARGIN,
+		backgroundColor: BUTTONCOLOR,
+	},
+	buttonText: {
+		fontSize: fontSize,
+		color: FONTCOLOR,
+	},
+});
+
+interface AlarmSettingsProps {
+	closeAlarmSettings: () => void;
+}
+
+interface AlarmSettingsState {
+	alarmSettings: SettingsData;
+	pressed: string;
+}
+
+export default class AlarmSettings extends Component<AlarmSettingsProps, AlarmSettingsState> {
+	private alarmSettings: SettingsData = {};
+
+	constructor(props: AlarmSettingsProps) {
+		super(props);
+		this.state = {
+			alarmSettings: {},
+			pressed: '',
+		};
+	}
+
+	public componentDidMount(): void {
+		AsyncStorage.getItem(ALARMSTORAGE)
+			.then(settings => {
+				if (settings) {
+					this.alarmSettings = JSON.parse(settings);
+					this.setState({ alarmSettings: this.alarmSettings });
+				}
+			})
+			.catch();
+	}
+
+	private onChangeText = (textInput: string, text: string) => {
+		this.alarmSettings[textInput] = text;
+	};
+
+	private onSave = () => {
+		if (this.alarmSettings[ALARMTIME].length === 4) {
+			this.alarmSettings[ALARMTIME] = '0' + this.alarmSettings[ALARMTIME];
+		}
+		AsyncStorage.setItem(ALARMSTORAGE, JSON.stringify(this.alarmSettings));
+		this.setState({ pressed: SAVE });
+		setTimeout(() => {
+			this.setState({ pressed: '' });
+		}, 250);
+	};
+
+	private onClose = () => {
+		this.props.closeAlarmSettings();
+		this.setState({ pressed: CLOSE });
+		setTimeout(() => {
+			this.setState({ pressed: '' });
+		}, 250);
+	};
+
+	public render(): JSX.Element | null {
+		return (
+			<Modal
+				animationType={'fade'}
+				transparent={true}
+				visible={true}
+				onRequestClose={this.props.closeAlarmSettings}
+			>
+				<View style={styles.container}>
+					<View style={styles.containerSettingss}>
+						<View style={styles.settingsRow}>
+							<Text style={styles.label}>Alarm time</Text>
+							<TextInput
+								style={styles.alarmInput}
+								spellCheck={false}
+								autoFocus={true}
+								onChangeText={text => this.onChangeText(ALARMTIME, text)}
+								key={this.state?.alarmSettings[ALARMTIME] || DEFAULTALARMTIME}
+								defaultValue={this.state?.alarmSettings[ALARMTIME] || DEFAULTALARMTIME}
+							/>
+						</View>
+						<View style={styles.buttonContainer}>
+							<Pressable
+								style={[
+									styles.button,
+									{
+										opacity: this.state.pressed === SAVE ? OPACITYPRESSED : undefined,
+									},
+								]}
+								onPress={this.onSave}
+							>
+								<Text style={styles.buttonText}>Save</Text>
+							</Pressable>
+							<Pressable
+								style={[
+									styles.button,
+									{
+										opacity: this.state.pressed === CLOSE ? OPACITYPRESSED : undefined,
+									},
+								]}
+								onPress={this.onClose}
+							>
+								<Text style={styles.buttonText}>Close</Text>
+							</Pressable>
+						</View>
+					</View>
+				</View>
+			</Modal>
+		);
+	}
+}
