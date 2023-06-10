e3d8fe0e1c228bebad00766f3e725e5711a22ed4
alariej <alaj@gmx.net>
2023-06-10 13:11:15 +0200

    Added global constants

---
+++ src/uiconfig.ts
@@ -0,0 +1,46 @@
+export const APPCOLOR = 'steelblue';
+export const FONTCOLOR = 'white';
+export const BUTTONCOLOR = 'rgba(0, 0, 0, 0.5)';
+export const PLACEHOLDERCOLOR = 'silver';
+export const MODALBACKGROUNDCOLOR = 'rgba(0, 0, 0, 0.7)';
+export const TEXTINPUTBACKGROUNDCOLOR = 'white';
+export const TEXTINPUTFONTCOLOR = 'dimgrey';
+export const FONTSIZE = 24;
+export const MARGIN = 24;
+export const OPACITYPRESSED = 0.2;
+export const DEFAULTSTREAMURL = 'https://radio4.cdm-radio.com:18020/stream-mp3-Chill';
+export const DEFAULTLOCATION = 'ZH';
+export const DEFAULTLATITUDE = '47.3492';
+export const DEFAULTLONGITUDE = '8.5654';
+export const DEFAULTALARMTIME = '06:30';
+export const DEFAULTALARMDURATION = 20;
+export const STREAMURL = 'stream_url';
+export const SETTINGSSTORAGE = 'settings';
+export const LOC1ID = 'loc_1_id';
+export const LOC2ID = 'loc_2_id';
+export const LOC3ID = 'loc_3_id';
+export const LOC4ID = 'loc_4_id';
+export const LOC1LAT = 'loc_1_lat';
+export const LOC2LAT = 'loc_2_lat';
+export const LOC3LAT = 'loc_3_lat';
+export const LOC4LAT = 'loc_4_lat';
+export const LOC1LON = 'loc_1_lon';
+export const LOC2LON = 'loc_2_lon';
+export const LOC3LON = 'loc_3_lon';
+export const LOC4LON = 'loc_4_lon';
+export const ALARMSTORAGE = 'alarm';
+export const ALARMTIME = 'alarm_time';
+export const SAVE = 'SAVE';
+export const CLOSE = 'CLOSE';
+export const PLAY = 'PLAY';
+export const STOP = 'STOP';
+export const ALARM = 'ALARM';
+export const SCREENSAVER = 'SCREENSAVER';
+export const SETTINGS = 'SETTINGS';
+export const LOCATIONIDPLACEHOLDER = 'ID (2-3 letters)';
+export const LATITUDEPLACEHOLDER = 'Latitude';
+export const LONGITUDEPLACEHOLDER = 'Longitude';
+
+export interface SettingsData {
+	[key: string]: string;
+}
