09e598cdaa956bef7cf59689900c6b13821158d1
alariej <alaj@gmx.net>
2023-06-14 10:43:40 +0200

    Added lint disable

--- electron/preload.ts
+++ electron/preload.ts
@@ -82,6 +82,7 @@ function useLoading() {
 
 // ----------------------------------------------------------------------
 
+// eslint-disable-next-line react-hooks/rules-of-hooks
 const { appendLoading, removeLoading } = useLoading()
 domReady().then(appendLoading)
 
